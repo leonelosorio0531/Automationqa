3c99613f75c955ce7e1cae764e9caf38c58acaa9
leonelosorio0531 <66289686+leonelosorio0531@users.noreply.github.com>
2020-09-03 22:19:50 -0300

    agregado ejemplo

---
+++ prueba1.py
@@ -0,0 +1,6 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+browser=webdriver.Firefox(executable_path="C:\\Users\\admin\\Desktop\\leo\\geckodriver.exe")
+browser.get("http://automationpractice.com/index.php")
+browser
